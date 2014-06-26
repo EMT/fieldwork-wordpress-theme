d6f1449fa1af31d2bac50ea3030b97a70a76c48f
Harry Parton <hparton@hotmail.co.uk>
2014-06-26 16:21:13 +0100

    Add in index warning message.

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+//	Fieldwork Blog Theme
+//	Version - 1.0
+// 
+//	This file should be empty, its a crime to edit it but hey, using wordpress is already a crime.
+//	In for a penny, in for a pound!
+
+?>
