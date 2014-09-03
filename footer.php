8107db88f669cb08cb599f302aaf8315fe134ee1
Harry Parton <hparton@hotmail.co.uk>
2014-09-03 15:03:20 +0100

    Remove scripts from footer.php

---
+++ footer.php
@@ -0,0 +1,16 @@
+</div>
+	<footer id="footer">
+		<div>
+			<ul class="social">
+				<li><a class="" href="http://twitter.com/madebyfieldwork">Twitter</a></li>
+				<li><a class="" href="http://facebook.com/madebyfieldwork">Facebook</a></li>
+				<li><a class="" href="http://flickr.com/photos/madebyfieldwork">Flickr</a></li>
+				<li><a class="" href="https://github.com/EMT">Github</a></li>
+				<li><a class="" href="http://vimeo.com/madebyfieldwork">Vimeo</a></li>
+			</ul>
+			<p>22 Lever St Manchester M1 1EA | hello@madebyfieldwork.com | <a href="tel:01612355318">0161 235 5318</a></p>
+		</div>
+	</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
