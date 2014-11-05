37dacd5f1a5656bf4a49c844c5d9e35a8b238d30
Harry Parton <hparton@hotmail.co.uk>
2014-11-05 17:20:23 +0000

    Fix Category highlighting on posts, and add in nav links.

--- single.php
+++ single.php
@@ -7,7 +7,7 @@
 		<div class="text">
 			<div class="wrapper">
 				<div class="project-title">
-					<h1 class="cat-hightlight"><?php the_title(); ?></h1>
+					<h1 class="cat-highlight"><?php the_title(); ?></h1>
 					<p>Posted on <?php the_time('F j Y'); ?></p>
 					<p class="post-author">Posted by <span class="author-link"><?php the_author_posts_link(); ?></span></p>
 					<div class="project-social">
@@ -19,17 +19,12 @@
 						</ul>
 					</div>
 				</div>
-				<div class="content-section-body">
-
-					<div class="blog-content">
-						<?php the_content(); ?>
-					</div>
+				<div class="content-section-body blog-content">
+					<?php the_content(); ?>
 				</div>
 				<div class="related-posts">
-					<p class="link-title">Next Post</p>
-					<a href="#">Animating a Virtual Journey</a>
-					<p class="link-title">Previous Post</p>
-					<a href="#">Front-end Developers we need you</a>
+					<?php next_post_link('<p class="link-title">Next Post</p> %link'); ?> 
+					<?php previous_post_link('<p class="link-title">Previous Post</p> %link'); ?> 
 				</div>
 			</div>	
 		</div>
