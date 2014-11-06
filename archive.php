54de0e2bc4f546778902be57390daa93244b17af
Harry Parton <hparton@hotmail.co.uk>
2014-11-06 15:44:10 +0000

    Update archive and home template files.

--- archive.php
+++ archive.php
@@ -1,48 +1,37 @@
 
 <?php get_header(); ?>
-
-
-<?php get_sidebar(); ?>
-<div class="main-content">
+<div class="wrapper">
 	<ul class="post-list">
-		
- <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<li>
-			<article class="post<?php the_category_unlinked(' '); ?>">
-				<a href="<?php the_permalink() ?>">
-				<?php if ( has_post_thumbnail() ) { ?>
-					<div class="post-image">
-					<?php the_post_thumbnail(); ?>
-					</div>
-				<?php } else { ?>
-					<div class="post-image no-image">
-						<h5 class="category"><?php echo get_cat_name( get_post_meta($post->ID, 'mob_category_prefered', true) ) ?></h5>
-						<p><?php the_excerpt(); ?></p>
-					</div>
-				<?php } ?>
-				</a>
-				<div class="post-meta-data">
-					<span class="date"><?php the_time('F j'); ?></span>
-					<a href="<?php the_permalink() ?>"><h2 class="title"><?php the_title(); ?><?php if ( get_post_meta($post->ID, 'title_long', true) ) : ?><span class="title-long"> - <?php echo get_post_meta($post->ID, 'title_long', true); ?></span><?php endif; ?></h2></a>
-					<span class="go-to"><a href="<?php the_permalink() ?>"></a></span>
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<li>
+				<article class="post<?php the_category_unlinked(' '); ?>">
+					<a href="<?php the_permalink() ?>">
+						<?php if ( has_post_thumbnail() ) { ?>
+							<div style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>'); background-size: cover;" class="post-image"></div>
+						<?php } else { ?>
+							<div class="post-image no-image"></div>
+						<?php } ?>
+					</a>
 					<ul class="category-icons">
 						<?php 
 						foreach((get_the_category()) as $category) { 
-						    echo "<li class='" . $category->slug . "'><a class='tag' href='" . get_category_link($category->term_id ) . "'></a></li>";
+							echo "<li class='" . $category->slug . "'><a class='tag' href='" . get_category_link($category->term_id ) . "'></a></li>";
 						} 
 						?>
 					</ul>
-				</div>
-			</article>
-		</li>
-	<?php endwhile;?>
-	<div class="pagination">
-	<span class="next"><?php next_posts_link('next'); ?></span>
-	<span class="prev"><?php previous_posts_link('previous'); ?></span>
-	</div>
+					<div class="post-meta-data">
+						<a href="<?php the_permalink() ?>"><h2 class="title"><?php the_title(); ?></h2></a>
+						<span class="date"><?php the_time('j F Y'); ?></span>
+						<span class="author">Posted by <span class="author-link"><?php the_author_posts_link(); ?></span></span>
+					</div>
+				</article>
+			</li>
+		<?php endwhile;?>
+		<div class="pagination">
+			<span class="next"><?php next_posts_link('next'); ?></span>
+			<span class="prev"><?php previous_posts_link('previous'); ?></span>
+		</div>
 	<?php endif; ?> 
-	</ul>
+</ul>
 </div>
-<?php get_sidebar(); ?>
-
 <?php get_footer(); ?>
