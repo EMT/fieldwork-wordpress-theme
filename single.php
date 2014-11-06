17721f008efa2de26e3bf4f7b8ba62ac3a2e23d1
Harry Parton <hparton@hotmail.co.uk>
2014-11-06 15:46:35 +0000

    Add in support for no-image on blog posts.

--- single.php
+++ single.php
@@ -2,13 +2,13 @@
 
 
 <?php while (have_posts()) : the_post(); ?>
-	<div style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>'); background-size: cover;" class="hero-picture"></div>
+	<div <?php if ( has_post_thumbnail() ) { ?>style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>'); background-size: cover;" class="hero-picture"<?php } else { ?> class="hero-picture no-image" <?php } ?>></div>
 	<section class="hero">
 		<div class="text">
 			<div class="wrapper">
 				<div class="project-title">
 					<h1 class="cat-highlight"><?php the_title(); ?></h1>
-					<p>Posted on <?php the_time('F j Y'); ?></p>
+					<p>Posted on <?php the_time('j F Y'); ?></p>
 					<p class="post-author">Posted by <span class="author-link"><?php the_author_posts_link(); ?></span></p>
 					<div class="project-social">
 						<ul>
