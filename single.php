eab90a15e4b321fcea79e656db2cf41e42dbfded
Harry Parton <hparton@hotmail.co.uk>
2014-11-17 11:51:24 +0000

    Swap to new classes on single layout.

--- single.php
+++ single.php
@@ -1,12 +1,11 @@
 <?php get_header(); ?>
-
+<div class="page-section">
 
 <?php while (have_posts()) : the_post(); ?>
-	<div <?php if ( has_post_thumbnail() ) { ?>style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>'); background-size: cover;" class="hero-picture"<?php } else { ?> class="hero-picture no-image" <?php } ?>></div>
 	<section class="hero">
 		<div class="text">
 			<div class="wrapper">
-				<div class="project-title">
+				<div class="section-title">
 					<h1 class="cat-highlight"><?php the_title(); ?></h1>
 					<p>Posted on <?php the_time('j F Y'); ?></p>
 					<p class="post-author">Posted by <span class="author-link"><?php the_author_posts_link(); ?></span></p>
@@ -19,7 +18,7 @@
 						</ul>
 					</div>
 				</div>
-				<div class="content-section-body blog-content">
+				<div class="section-text blog-content">
 					<?php the_content(); ?>
 				</div>
 				<div class="related-posts">
@@ -30,4 +29,5 @@
 		</div>
 	</section>
 <?php endwhile ?>
+</div>
 <?php get_footer(); ?>
