9b278eb3121b6df3a3bf46e9baa8aa61b3dd399a
Harry Parton <hparton@hotmail.co.uk>
2014-11-17 11:50:35 +0000

    Remove some comments in index.php

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 
 //	Fieldwork Blog Theme
 //	Version - 1.0
-// 
-//	This file should be empty, its a crime to edit it but hey, using wordpress is already a crime.
-//	In for a penny, in for a pound!
 
 ?>
 
