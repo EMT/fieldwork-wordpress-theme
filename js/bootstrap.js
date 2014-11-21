32db8353d6be6884657034bf3068c2daf77947e3
Harry Parton <hparton@hotmail.co.uk>
2014-11-21 10:52:50 +0000

    Remove some outdated JS

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,23 +1,3 @@
-$(window).on('scroll', function() {
-	// Opacity for the header on project pages
-	var hero_height = $('.hero-picture').height(),
-	header = $('.project header');
-
-	if ($(window).scrollTop() > hero_height) {
-		header.addClass('visible');
-	}
-	else{
-		header.removeClass('visible');
-	}
-});
-
-// $(window).scroll(function() {
-// 	if ( $(window).outerWidth() >= 900 ) {
-// 		var scrolledY = $(window).scrollTop();
-// 		$('.hero-picture').css('transform', 'translateY(' + ((scrolledY)) + 'px)');
-// 	}
-// });
-
 $('.blog-video').on('click', function(){ 
 	var videoSource = $(this).children('.play').data('video');
 	$(this).children('img').fadeOut(750);
@@ -32,17 +12,5 @@ $('.blog-video').on('click', function(){
 });
 
 
-// Fix for the sidebar showing on mobile (looking into a css only version)
-// Sets the height of the main content to the height of the window if its too short.
-
-$(window).on('resize load', function(){
-
-	var contentHeight = $('.main-content').height();
-	var windowHeight = $(window).innerHeight();
-
-	if ( windowHeight > contentHeight ) {
-		$('.main-content').height(windowHeight);
-	}
-});
 
 
