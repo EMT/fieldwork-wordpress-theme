c2bd9c5c95ed3cb38bca6e01364138b848209043
Harry Parton <hparton@hotmail.co.uk>
2014-12-03 11:08:13 +0000

    Change categories title to 'Topics'.

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <div class="sidebar js-sidebar">
 <a href="#" class="cross js-sidebar-close">✕</a>
-	<h3>Categories</h3>
+	<h3>Topics</h3>
 	<ul class="category-links">
 		<?php wp_list_categories('hide_empty=0&title_li='); ?> 
 	</ul>
