26dc1037eeacfbcd788dc587ed1ab9f88c80c322
Andy Gott <andy@madebyfieldwork.com>
2014-12-05 19:59:48 +0000

    Add Analytics tracking code.

--- footer.php
+++ footer.php
@@ -41,5 +41,21 @@
 <?php wp_footer(); ?>
 <script type='text/javascript' src='<?php echo get_template_directory_uri(); ?>/vendor/highlight.pack.js'></script>
 <script>hljs.initHighlightingOnLoad();</script>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-34217297-1']);
+  _gaq.push(['_setDomainName', 'madebyfieldwork.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
 </body>
 </html>
