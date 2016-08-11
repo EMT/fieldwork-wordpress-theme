be66dccd2a125cd9f2f9d223f6f3af019e8c929c
Andy Gott <andy@madebyfieldwork.com>
2016-08-11 12:42:59 +0100

    Fix issues with links and videos in image galleries.

--- functions.php
+++ functions.php
@@ -93,7 +93,7 @@ function add_theme_scripts() {
     wp_enqueue_script( 'bquery', get_template_directory_uri() . '/js/jquery.min.js','','',true);
     wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/js/modernizr.js','','',true);
     wp_enqueue_script( 'touchSwipe', get_template_directory_uri() . '/js/jquery.touchSwipe.js','','',true);
-    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.js','','',true);
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.js','','123',true);
 }
 
 
@@ -144,11 +144,14 @@ function imageGallery($atts, $content = null) {
                     }
 
                     // Links to media file / external site.
-                    if ( get_sub_field('media') == 'true' ) {
-                        $preLinkWrapper = '<a href="'.$link.'"/>';
+                    if ( get_sub_field('video_url') ) {
+                        $preLinkWrapper = '<a href="'.get_sub_field('video_url').'" target="_blank"/>';
                         $postLinkWrapper = '</a>';
                     } else if ( get_sub_field('link_url') ) {
                         $preLinkWrapper = '<a href="'.get_sub_field('link_url').'" target="_blank"/>';
+                        $postLinkWrapper = '</a>';
+                    } else if ( get_sub_field('media') == 'true' ) {
+                        $preLinkWrapper = '<a href="'.$link.'"/>';
                         $postLinkWrapper = '</a>';
                     } else {
                         $preLinkWrapper = '';
--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,17 +1,16 @@
-$('.blog-video').on('click', function(){ 
-	var videoSource = $(this).children('.play').data('video');
-	$(this).children('img').fadeOut(750);
-	if ( $(this).children('.iframe-wrapper').length > 0 ) {
-		return;
-	} else {
-		$(this).prepend('<div class="iframe-wrapper"><iframe src="' + videoSource + '" frameborder="0" autoplay="true" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>');
-	}
-	$(this).children('.iframe-wrapper').fadeIn();
-	$(this).children('img').addClass('video-playing');
-	$(this).children('.play').hide();
-});
-
-
+// $('.blog-video').on('click', function(){ 
+// 	e.preventDefault();
+// 	var videoSource = $(this).children('.play').data('video');
+// 	$(this).children('img').fadeOut(750);
+// 	if ( $(this).children('.iframe-wrapper').length > 0 ) {
+// 		return;
+// 	} else {
+// 		$(this).prepend('<div class="iframe-wrapper"><iframe src="' + videoSource + '" frameborder="0" autoplay="true" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>');
+// 	}
+// 	$(this).children('.iframe-wrapper').fadeIn();
+// 	$(this).children('img').addClass('video-playing');
+// 	$(this).children('.play').hide();
+// });
 
 
 $('.js-reveal').click( function(e){
